278819f862d661473b0b24277437e44a55848115
josuelfeitosa <josuelfeitosa@gmail.com>
2023-01-29 18:51:59 -0300

    Left menu

--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -28,6 +28,72 @@ class UI_MainWindow(object):
         self.left_menu.setMinimumWidth(50)
         self.left_menu.setMaximumWidth(50)
         
+        # LEFT MENU LAYOUT
+        self.left_menu_layout = QVBoxLayout(self.left_menu)
+        self.left_menu_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_layout.setSpacing(0)
+        
+        # TOP FRAME MENU
+        self.left_menu_top_frame = QFrame()
+        self.left_menu_top_frame.setMinimumHeight(50)
+        self.left_menu_top_frame.setObjectName("left_menu_top_frame")
+        self.left_menu_top_frame.setStyleSheet(
+            "#left_menu_top_frame { background-color: red; }"
+        )
+        
+        # TOP FRAME MENU LAYOUT
+        self.left_menu_top_layout = QVBoxLayout(self.left_menu_top_frame)
+        self.left_menu_top_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_top_layout.setSpacing(0)
+        
+        # TOP BTNS
+        self.toggle_button = QPushButton("Toggle")
+        self.btn_1 = QPushButton("Button 1")
+        self.btn_2 = QPushButton("Button 2")
+        
+        # ADD BTNS TO LAYOUT
+        self.left_menu_top_layout.addWidget(self.toggle_button)
+        self.left_menu_top_layout.addWidget(self.btn_1)
+        self.left_menu_top_layout.addWidget(self.btn_2)
+        
+        # MENU SPACER
+        self.left_menu_spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        
+        # BOTTOM FRAME MENU
+        self.left_menu_bottom_frame = QFrame()
+        self.left_menu_bottom_frame.setMinimumHeight(50)
+        self.left_menu_bottom_frame.setObjectName("left_menu_bottom_frame")
+        self.left_menu_bottom_frame.setStyleSheet(
+            "#left_menu_bottom_frame { background-color: red; }"
+        )
+        
+        # BOTTOM FRAME MENU LAYOUT
+        self.left_menu_bottom_layout = QVBoxLayout(self.left_menu_bottom_frame)
+        self.left_menu_bottom_layout.setContentsMargins(0, 0, 0, 0)
+        self.left_menu_bottom_layout.setSpacing(0)
+        
+        # BOTTOM BTN
+        self.settings_btn = QPushButton("Settings")
+        
+        # ADD BTNS TO LAYOUT
+        self.left_menu_bottom_layout.addWidget(self.settings_btn)
+        
+        # LABEL VERSION
+        self.left_menu_label_version = QLabel("v1.0.0.0")
+        self.left_menu_label_version.setAlignment(Qt.AlignCenter)
+        self.left_menu_label_version.setMinimumHeight(30)
+        self.left_menu_label_version.setMaximumHeight(30)
+        self.left_menu_label_version.setStyleSheet(
+            "color: #c3ccdf"
+        )
+        
+        # ADD TO LAYOUT
+        self.left_menu_layout.addWidget(self.left_menu_top_frame)
+        self.left_menu_layout.addItem(self.left_menu_spacer)
+        self.left_menu_layout.addWidget(self.left_menu_bottom_frame)
+        self.left_menu_layout.addWidget(self.left_menu_label_version)
+        
+        
         # CONTENT
         self.content = QFrame()
         self.content.setStyleSheet(
