643bb4efbe54f6cb3b99bf40f1a4b80f2e49d12b
josuelfeitosa <josuelfeitosa@gmail.com>
2023-01-30 14:11:48 -0300

    Left menu animation

--- main.py
+++ main.py
@@ -15,8 +15,29 @@ class MainWindow(QMainWindow):
         self.ui = UI_MainWindow()
         self.ui.setup_ui(self)
         
+        #Toggle button
+        self.ui.toggle_button.clicked.connect(self.toggle_button)
+        
+        
         # EXIBI A NOSSA APLICAÇÃO
         self.show()
+        
+    def toggle_button(self):
+        # Get menu width
+        menu_width = self.ui.left_menu.width()
+        
+        # Check with
+        width = 50
+        if menu_width == 50:
+            width = 240
+            
+        # Start animation
+        self.animation = QPropertyAnimation(self.ui.left_menu, b"minimumWidth")
+        self.animation.setStartValue(menu_width)
+        self.animation.setEndValue(width)
+        self.animation.setDuration(500)
+        self.animation.setEasingCurve(QEasingCurve.InOutCirc)
+        self.animation.start()
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
